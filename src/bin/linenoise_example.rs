35209a573a95a95613f63c5881def2f3f63c37dc
Pierre Baillet <pierre@baillet.name>
2014-12-13 13:55:56 +0100

    Latest changes to comply with rust change

--- src/bin/linenoise_example.rs
+++ src/bin/linenoise_example.rs
@@ -3,6 +3,38 @@ extern crate libc;
 
 use std::c_str;
 
+struct Completion {
+	completions: *mut *mut i8,
+	cb: *mut fn(&str) -> Vec<&str>
+}
+
+impl Completion {
+	fn input(&self, prompt: &str) -> Option<String> {
+
+	    return linenoise::linenoise(prompt);
+	}
+	fn set_callback(&mut self, callback:fn(&str) -> Vec<&str>) {
+		self.cb = callback as *mut _;
+	    linenoise::set_completion_callback(cb);
+	}
+	fn cb(cs: *mut libc::c_char, lc:*mut linenoise::Completions ) {
+		let input: Option<&str>;
+		let ccurrent_input: std::c_str::CString;
+
+		unsafe {
+			ccurrent_input = c_str::CString::new(cs as *const _, false);
+			input = ccurrent_input.as_str();
+		}
+		match input {
+			None => { return; }
+			_ => {
+				let ip = input.unwrap();
+				println!("Input: {}", ip);
+			}
+		}
+	}
+}
+
 fn cb(cs: *mut libc::c_char, lc:*mut linenoise::Completions ) {
 	let input: Option<&str>;
 	let ccurrent_input: std::c_str::CString;
@@ -18,14 +50,18 @@ fn cb(cs: *mut libc::c_char, lc:*mut linenoise::Completions ) {
 		}
 	}
 
-	let rep = "coucou".to_c_str();
-	let adr = rep.as_ptr();
-
-	let mut vec = Vec::new();
-
-	vec.push(adr);
-
 	unsafe {
+
+		let tep = "coucou".to_c_str();
+		let rep = tep.as_ptr();
+
+		let tap = "kiki".to_c_str();
+		let rap = tap.as_ptr();
+
+		let top = "koko".to_c_str();
+		let rop = top.as_ptr();
+
+		let mut vec = vec![rep, rap, rop];
 		(*lc).len = 1;
 		(*lc).cvec = vec.as_mut_ptr() as *mut *mut i8;
 	}
@@ -34,6 +70,8 @@ fn cb(cs: *mut libc::c_char, lc:*mut linenoise::Completions ) {
 
 fn main() {
 	println!("Youhou.");
+	let completion = Completion{ completions: 0 as *mut *mut i8, cb: 0 as *mut fn(&str) -> Vec<&str>  };
+
     linenoise::set_completion_callback(cb);
     loop {
 	    let val = linenoise::linenoise("hello > ");
