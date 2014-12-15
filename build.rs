2110fbdfaf2769a1ac5a5cd073c3c04045f2445a
Pierre Baillet <pierre@baillet.name>
2014-12-15 16:03:58 +0100

    Guess debugging

--- build.rs
+++ build.rs
@@ -3,5 +3,5 @@ extern crate gcc;
 use std::default::Default;
 
 fn main() {
-    gcc::compile_library("liblinenoise.a", &Default::default(),  &["native/linenoise.c"]);
+    // gcc::compile_library("liblinenoise.a", &Default::default(),  &["native/linenoise.c"]);
 }
