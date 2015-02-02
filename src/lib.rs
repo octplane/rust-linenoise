5457c9cb55f51dbb510c01ade3b2b5c4d3ba7a71
Pierre Baillet <pierre@baillet.name>
2015-02-02 11:41:03 +0100

    Fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,47 @@
+#![crate_name="linenoise"]
 #![crate_type="lib"]
 #![feature(slicing_syntax)]
 #![feature(int_uint)]
 #![feature(libc, std_misc, collections, core)]
+
+//! This is a library that interfaces with the linenoise library.
+//! [Linenoise](https://github.com/antirez/linenoise) is a library implemented by Antirez, the Redis creator as a
+//! replacement for readline.
+//!
+//! This library is just a binding to this library.
+//!
+//! ```rust
+//! extern crate linenoise;
+//! fn callback(input: &str) -> Vec<String> {
+//!   let mut ret : Vec<&str>;
+//!    if input.starts_with("s") {
+//!     ret = vec!["suggestion", "suggestion2", "suggestion-three"];
+//!   } else {
+//!     ret = vec!["wot"];
+//!   }
+//!
+//!     return ret.iter().map(|s| s.to_string()).collect();
+//! }
+//!
+//! fn main() {
+//!   linenoise::set_callback(callback);
+//!
+//!     loop {
+//!       let val = linenoise::input("hello > ");
+//!         match val {
+//!             None => { break }
+//!             Some(input) => {
+//!                 println!("> {}", input);
+//!                 linenoise::history_add(input.as_slice());
+//!                 if input.as_slice() == "clear" {
+//!                   linenoise::clear_screen();
+//!                 }
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
+
 
 extern crate libc;
 
