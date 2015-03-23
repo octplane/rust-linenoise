5cc69beba8263817072183e2b30bc30c2097868b
Pierre Baillet <pierre@baillet.name>
2015-03-23 09:47:50 +0100

    Fix for latest rust

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 extern crate gcc;
 
-use std::default::Default;
-
 fn main() {
-	gcc::compile_library("liblinenoise.a", &Default::default(),  &["native/linenoise.c"]);
+	gcc::compile_library("liblinenoise.a", &["native/linenoise.c"]);
 }
